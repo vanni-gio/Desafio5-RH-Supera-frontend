f7767777cc09cf880d6203264421cfd838901f43
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-16 13:29:29 -0300

    realocando styles do App.css

---
+++ src/components/FiltroTransferencia/styles.ts
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+
+
+export const Form = styled.form`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const ContainerForm = styled.div`
+    margin-top: 2%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
