63123b9b053b2e2e75de5f3fdbfc6a2c91b66c93
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-16 17:56:22 -0300

    componentizando elementos

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,53 @@ import './App.css';
 import { getTransferencias } from './api/api'
 import { TabelaTransferencias } from './components/TabelaTransferencias';
 import { Transferencia } from './types/Transferencia';
-import { FiltroTransferencia } from './components/FiltroTransferencia';
+import { FiltroTransferenciaForm } from './components/FiltroTransferenciaForm';
+import { SectionSaldos } from './components/SectionSaldos';
 
-function App() {
-  const [transferencias, setTransferencias] = useState<Transferencia[]>([]);
-
-  useEffect(() => {
-      getTransferencias().then((resp) => {
-        setTransferencias(resp.data);
+const getAndSetDados = (
+    setT: React.Dispatch<React.SetStateAction<Transferencia[]>>,
+    setS: React.Dispatch<React.SetStateAction<{
+    saldoTotal: number;
+    saldoPeriodo: number;
+  } | undefined>>,
+    filtro?: string) => {
+      getTransferencias(filtro).then((resp) => {
+        setT(resp.data.transferencias);
+        setS({saldoPeriodo: resp.data.saldoPeriodo, saldoTotal: resp.data.saldoTotal});
       }).catch((error) => {
         console.log(error);
       });
+}
 
-  }, [])
+
+
+function App() {
+  const [transferencias, setTransferencias] = useState<Transferencia[]>([]);
+  const [saldos, setSaldos] = useState<{saldoTotal: number, saldoPeriodo: number}>();
+
+
+  const handleSubmit = (filtro: string) => {    
+    getAndSetDados(setTransferencias, setSaldos, filtro);
+  }
+
+
+  useEffect(() => {
+    getAndSetDados(setTransferencias, setSaldos);
+  }, [getAndSetDados])
   
+
+
   return (
     <div className="page-container">
       <header>
         <h1 style={{color: 'seagreen', textShadow: '2px -1px darkseagreen'}}>Banco App</h1>
-        <FiltroTransferencia></FiltroTransferencia>
+        <FiltroTransferenciaForm handleSubmit={handleSubmit}></FiltroTransferenciaForm>
         <hr/>
       </header>
       <main className='main-content'>
         <div className='container-transferencias'>
           <div className='container-table-transacoes'>
-              <section className='section-saldo'>
-                <div className='container-saldo'>
-                  <label className='label-saldo'>Saldo Total: </label>
-                  <span className='qtd-saldo'>R$ 50,00</span>
-                </div>
-                <div className='container-saldo'>
-                  <label className='label-saldo'>Saldo no Periodo: </label>
-                  <span className='qtd-saldo'>R$ 50,00</span>
-                </div>
-              </section>
+            <SectionSaldos saldoPeriodo={saldos ? saldos.saldoPeriodo : 0} saldoTotal={saldos ? saldos.saldoTotal : 0}></SectionSaldos>
             <TabelaTransferencias dadosTabela={transferencias}></TabelaTransferencias>
           </div>
         </div>
